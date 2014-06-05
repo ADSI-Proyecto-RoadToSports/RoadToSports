db2ef9159d9ca721874fe31110cc0f8718343877
JohnFreddy <jh0n.fr3ddym@gmail.com>
2014-06-05 03:26:24 -0500

    Estructura actualizada

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140528020157) do
+ActiveRecord::Schema.define(version: 20140605063610) do
+
+  create_table "document_types", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "inicios", force: true do |t|
+    t.string   "index"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "personas", force: true do |t|
     t.string   "nombre"
     t.string   "apellido"
     t.string   "telefono"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "remember_session"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
