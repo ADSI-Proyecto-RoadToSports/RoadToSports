0b0d197709a3df2b935051b3901652f31af56ea8
JuanSe12 <megajua_45@hotmail.com>
2014-08-20 04:44:11 -0500

    Cambios Pequeños en La Vista De Torneos

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,2 +1,9 @@
 module TournamentsHelper
+	def mostrar_estado(state)
+		if state
+			"<span class='label label-success'>Activo </span>".html_safe
+		else
+			"<span class='label label-danger'>Inactivo </span>".html_safe
+		end
+	end
 end
