953c6e94b928179ffe1ad959e396d1184c86829e
JuanSe12 <Megajuan_45@hotmail.com>
2014-09-09 13:08:03 -0500

    Cambios En El Archivo generador De Pdf

--- app/pdfs/report_pdf.rb
+++ app/pdfs/report_pdf.rb
@@ -8,34 +8,39 @@ class ReportPdf < Prawn::Document
 	end
   def titulo
   	text "Dato De Toda La Tabla De Partidos", size: 15, style: :bold, :align => :center, :color => [10, 81, 10, 80]
-  	text "_________________________________________________________\n", style: :bold, :align => :center
+  	text "________________________________________________________________________________\n", style: :bold, :align => :center
   end
   def cosas
   	con =0
+  	text " \n"
   	@pfutbols.map do |pfutbol|
   			con = con + 1
-  			text "Partido: #{con}"
-			text "Nombre Del Primer Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{pfutbol.equipo1}", size: 15, style: :normal, :align => :center
-			text "Goles Del Primer Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{pfutbol.puntos1}", size: 15, style: :normal, :align => :center
-			text "Puntos Del Torneo Del Primer Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{pfutbol.puntoso1}", size: 15, style: :normal, :align => :center
-			text "Nombre Del Segundo Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{pfutbol.equipo2}", size: 15, style: :normal, :align => :center
-			text "Goles Del Segundo Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{pfutbol.puntos2}", size: 15, style: :normal, :align => :center
-			text "Puntos Del Torneo Del Segundo Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{pfutbol.puntoso2}", size: 15, style: :normal, :align => :center
-			text "Nombre Del Torneo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{pfutbol.tournament.name}", size: 15, style: :normal, :align => :center
-			text "Fecha Del Partido:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{pfutbol.fecha}", size: 15, style: :normal, :align => :center
-			text "Estado Del Partido Actual:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{estado(pfutbol.estado)}", size: 15, style: :normal, :align => :center
-			text "Reponsable Del Partido:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "#{pfutbol.user.nombre}", size: 15, style: :normal, :align => :center
-			text "_________________________________________________________", style: :bold, :align => :center
+  			text " \n"
+			#text "Nombre Del Primer Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			text "Partido: #{con}", size: 10, style: :normal
+			text "|Equipo1:#{pfutbol.equipo1}  |Goles Equipo1:  #{pfutbol.puntos1}  |Puntos Equipo1:  #{pfutbol.puntoso1} ", size: 10, style: :normal
+			text "|Equipo2:  #{pfutbol.equipo2}  |Goles Equipo2:  #{pfutbol.puntos2}  |Puntos Equipo2:  #{pfutbol.puntoso2}", size: 10, style: :normal
+			text "|Torneo:  #{pfutbol.tournament.name}  |Fecha:  #{pfutbol.fecha}", size: 10, style: :normal
+			text "|Estado:  #{estado(pfutbol.estado)}  |Responsable:  #{pfutbol.user.nombre}", size: 10, style: :normal
+			#text "Goles Del Primer Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			#text "#{pfutbol.puntos1}", size: 15, style: :normal, :align => :center
+			#text "Puntos Del Torneo Del Primer Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			#text "#{pfutbol.puntoso1}", size: 15, style: :normal, :align => :center
+			#text "Nombre Del Segundo Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			#text "#{pfutbol.equipo2}", size: 15, style: :normal, :align => :center
+			#text "Goles Del Segundo Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			#text "#{pfutbol.puntos2}", size: 15, style: :normal, :align => :center
+			#text "Puntos Del Torneo Del Segundo Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			#text "#{pfutbol.puntoso2}", size: 15, style: :normal, :align => :center
+			#text "Nombre Del Torneo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			#text "#{pfutbol.tournament.name}", size: 15, style: :normal, :align => :center
+			#text "Fecha Del Partido:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			#text "#{pfutbol.fecha}", size: 15, style: :normal, :align => :center
+			#text "Estado Del Partido Actual:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			#text "#{estado(pfutbol.estado)}", size: 15, style: :normal, :align => :center
+			#text "Reponsable Del Partido:", :color => [22, 81, 79, 30], style: :bold, :align => :center
+			#text "#{pfutbol.user.nombre}", size: 15, style: :normal, :align => :center
+			text " \n"
   	end
   end
   def estado(est)
@@ -44,5 +49,7 @@ class ReportPdf < Prawn::Document
   	else
   		"Pendiente"
   	end
+  	
   end
+
 end
