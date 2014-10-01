9b660060e4186c10b5f38dcff2947434ac8b3f79
JuanSe12 <Megajuan_45@hotmail.com>
2014-10-01 17:13:12 -0500

    Vistas de Todos los partidos con generador de pdf(tablas)-ajax

--- app/pdfs/reportv_pdf.rb
+++ app/pdfs/reportv_pdf.rb
@@ -1,53 +1,70 @@
 class ReportvPdf < Prawn::Document
 	def initialize(pvoleibols)
-		super(top_margin: 70)
+		super(top_margin: 10)
+		logo
 		@pvoleibols = pvoleibols
-		titulo
-		cosas
-		
+		pvoleibol_details
+		ayuda
+		#logo_final
 	end
-  def titulo
-  	text "Datos De Toda La Tabla De Partidos Volibol", size: 15, style: :bold, :align => :center, :color => [10, 81, 10, 80]
-  	text "________________________________________________________________________________\n", style: :bold, :align => :center
+
+  def logo
+    logopath =  "#{Rails.root}/app/assets/images/logo.png"
+    image logopath, :width => 60, :height => 60, :position => :center
+    draw_text "Listado de Partidos de Voleibol", :at => [123, 670], size: 22
   end
-  def cosas
-  	con =0
-  	text " \n"
-  	@pvoleibols.map do |pvoleibol|
-  			con = con + 1
-  			text " \n"
-			#text "Nombre Del Primer Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			text "Partido: #{con}", size: 10, style: :normal
-			text "|Equipo1:#{pvoleibol.equipo1}  |Puntos Equipo1:  #{pvoleibol.puntos1}  |Puntos Torneo Equipo1:  #{pvoleibol.puntoso1} ", size: 10, style: :normal
-			text "|Equipo2:  #{pvoleibol.equipo2}  |Puntos Equipo2:  #{pvoleibol.puntos2}  |Puntos Torneo Equipo2:  #{pvoleibol.puntoso2}", size: 10, style: :normal
-			text "|Torneo:  #{pvoleibol.tournament.name}  |Fecha:  #{pvoleibol.fecha}", size: 10, style: :normal
-			text "|Estado:  #{estado(pvoleibol.estado)}  |Responsable:  #{pvoleibol.user.nombre}", size: 10, style: :normal
-			#text "Goles Del Primer Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			#text "#{pvoleibol.puntos1}", size: 15, style: :normal, :align => :center
-			#text "Puntos Del Torneo Del Primer Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			#text "#{pvoleibol.puntoso1}", size: 15, style: :normal, :align => :center
-			#text "Nombre Del Segundo Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			#text "#{pvoleibol.equipo2}", size: 15, style: :normal, :align => :center
-			#text "Goles Del Segundo Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			#text "#{pvoleibol.puntos2}", size: 15, style: :normal, :align => :center
-			#text "Puntos Del Torneo Del Segundo Equipo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			#text "#{pvoleibol.puntoso2}", size: 15, style: :normal, :align => :center
-			#text "Nombre Del Torneo:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			#text "#{pvoleibol.tournament.name}", size: 15, style: :normal, :align => :center
-			#text "Fecha Del Partido:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			#text "#{pvoleibol.fecha}", size: 15, style: :normal, :align => :center
-			#text "Estado Del Partido Actual:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			#text "#{estado(pvoleibol.estado)}", size: 15, style: :normal, :align => :center
-			#text "Reponsable Del Partido:", :color => [22, 81, 79, 30], style: :bold, :align => :center
-			#text "#{pvoleibol.user.nombre}", size: 15, style: :normal, :align => :center
-			text " \n"
-  	end
+
+  #def logo_final
+  #	move_down 260
+  #	logopath =  "#{Rails.root}/app/assets/images/logo.png"
+  #  image logopath, :width => 60, :height => 60, :position => :center
+  #end
+
+  def ayuda
+  	move_down 10
+  	text "En la tabla de partidos de voleibol vemos un campo abreviado el cual significa lo siguiente:", size: 15, style: :bold, :align => :center, :color => [10, 81, 10, 80]
+  	text "-Com: significa estado del partido completo", size: 15, :align => :center, :color => [10, 20, 10, 80]
+  	text "-Ped: significa estado del partido pendiente", size: 15, :align => :center, :color => [10, 20, 10, 80]
+  	text "-una fila tiene como titulo Resp, esto significa responsable", size: 15, :align => :center, :color => [10, 20, 10, 80]
+  	text "-dos filas tienen como titulo Antc, esto significa anotaciones", size: 15, :align => :center, :color => [10, 20, 10, 80]
   end
+  
+  def pvoleibol_rows
+    [["Equipo1", "Antc", "Puntos Torneo", "Equipo2", "Antc", "Puntos Torneo", "Fecha", "Estado", "Resp", "Torneo"]] +
+    @pvoleibols.map do |pvoleibol|
+      [ "#{pvoleibol.equipo1} ",
+        "#{pvoleibol.puntos1} ",
+        "#{pvoleibol.puntoso1} ",
+        "#{pvoleibol.equipo2} ",
+        "#{pvoleibol.puntos2} ",
+        "#{pvoleibol.puntoso2} ",
+        "#{pvoleibol.fecha} ",
+        "#{estado(pvoleibol.estado)} ",
+        "#{pvoleibol.user.nombre} ",
+        "#{pvoleibol.tournament_id} "
+      ]
+    end
+  end
+
+  def pvoleibol_details
+    move_down 40
+    table pvoleibol_rows, :width => 558 do
+      row(0).font_style = :bold
+      row(0).border_top_width = 3
+      row(-1).border_bottom_width = 3
+      column(0).border_left_width = 3
+      column(-1).border_right_width = 3
+      columns(1..3).align = :left
+      self.header = true
+      self.column_widths = {0 => 60, 1 => 49, 2 => 50, 3 => 60, 4 => 49, 5 => 50, 6 => 72, 7 => 50, 8 => 68, 9 => 50}
+    end
+  end
+
   def estado(est)
   	if est
-  		"Terminado"
+  		"Com"
   	else
-  		"Pendiente"
+  		"Ped"
   	end
   	
   end
